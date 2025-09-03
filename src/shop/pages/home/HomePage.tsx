95937803ce6e5551c51ee4ecc09dcb9198f2e668
vista-juan-constantine <juan.constantine@vistadash.com>
2025-09-03 12:01:26 -0500

    Install  the ui and talwind

--- src/shop/pages/home/HomePage.tsx
+++ src/shop/pages/home/HomePage.tsx
@@ -1,6 +1,12 @@
+import { Button } from "@/components/ui/button";
 
 export const HomePage = () => {
   return (
-    <div>HomePages</div>
-  )
-}
+    <div>
+      <h1 className="font-monserrat font-thin">Hola Mundo</h1>
+      <h1 className="font-monserrat font-normal">Hola Mundo</h1>
+      <h1 className="font-monserrat font-bold">Hola Mundo</h1>
+      <Button>Hola soy un boton</Button>
+    </div>
+  );
+};
