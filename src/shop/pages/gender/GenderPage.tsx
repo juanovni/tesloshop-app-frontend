a3c86f038c682a394cb1651f3fb604f9789a67f4
vista-juan-constantine <juan.constantine@vistadash.com>
2025-09-07 11:07:46 -0500

    Added the adictional filters

--- src/shop/pages/gender/GenderPage.tsx
+++ src/shop/pages/gender/GenderPage.tsx
@@ -1,12 +1,12 @@
 import { useParams } from "react-router";
 import { CustomPagination } from "@/components/custom/CustomPagination";
-import { products } from "@/mocks/products.mock";
 import { CustomJumbotron } from "@/shop/components/CustomJumbotron";
 import { ProductGrid } from "@/shop/components/ProductGrid";
+import { useProducts } from "@/shop/hooks/useProducts";
 
 export const GenderPage = () => {
   const { gender } = useParams();
-
+  const { data: productsResponse } = useProducts();
   const _renderGenderLabel = (title: string) => {
     let label = "";
     switch (title) {
@@ -27,8 +27,8 @@ export const GenderPage = () => {
       <CustomJumbotron
         tittle={`Productos para ${_renderGenderLabel(gender || "")}`}
       />
-      <ProductGrid products={products} />
-      <CustomPagination totalPages={7} />
+      <ProductGrid products={productsResponse?.products || []} />
+      <CustomPagination totalPages={productsResponse?.pages || 1} />
     </>
   );
 };
