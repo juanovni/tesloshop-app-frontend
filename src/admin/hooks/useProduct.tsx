09efbdcefcb3038531f05e8ac2a20152aa7b54e6
vista-juan-constantine <juan.constantine@vistadash.com>
2025-09-08 12:34:33 -0500

    Installl the useForm and use the productform

---
+++ src/admin/hooks/useProduct.tsx
@@ -0,0 +1,17 @@
+import { useQuery } from "@tanstack/react-query";
+import { getProductByIdAction } from "../actions/get-product-by-id.action";
+import type { Product } from "@/interfaces/product.interface";
+
+export const useProduct = (id: string) => {
+  const query = useQuery({
+    queryKey: ["product", { id }],
+    queryFn: () => getProductByIdAction(id),
+    retry: false,
+    staleTime: 1000 * 60 * 5, // 5 minutos
+    // enabled: !!id
+  });
+
+  return {
+    ...query,
+  };
+};
