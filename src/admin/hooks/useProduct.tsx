7e22bc8783362c78f1285532f52a4fa280cdaab0
vista-juan-constantine <juan.constantine@vistadash.com>
2025-09-09 10:16:54 -0500

    Added the invalidateQueries to removed cache

--- src/admin/hooks/useProduct.tsx
+++ src/admin/hooks/useProduct.tsx
@@ -1,9 +1,11 @@
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { getProductByIdAction } from "../actions/get-product-by-id.action";
 import type { Product } from "@/interfaces/product.interface";
 import { createUpdateProductAction } from "../actions/create-update-product.action";
 
 export const useProduct = (id: string) => {
+  const queryClient = useQueryClient();
+
   const query = useQuery({
     queryKey: ["product", { id }],
     queryFn: () => getProductByIdAction(id),
@@ -13,11 +15,23 @@ export const useProduct = (id: string) => {
   });
 
   //  Mutacion
-  // Mas de una mutacion 
+  // Mas de una mutacion
   // const productMutation =
   const mutation = useMutation({
     mutationFn: createUpdateProductAction,
     onSuccess: (productLike: Product) => {
+      // INVALIDAR CACHE
+      queryClient.invalidateQueries({ queryKey: ["products"] }); // Todos Sirve para envalidar el cache para que se guarde de forma correcta
+      queryClient.invalidateQueries({
+        queryKey: ["product", { id: productLike.id }],
+      }); // Refresque el producto unico de cache
+
+      // VALIDAR CACHE PARA AHORRAR UNA LLAMADA AL API
+      queryClient.setQueryData(
+        ["products", { id: productLike.id }],
+        productLike
+      );
+
       console.log("Todo salio bien", productLike);
     },
   });
