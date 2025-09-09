5db615f065c0c6be16be76068bd6b34f944621a4
vista-juan-constantine <juan.constantine@vistadash.com>
2025-09-09 14:54:54 -0500

    Added the logic to upload the images

--- src/admin/actions/create-update-product.action.ts
+++ src/admin/actions/create-update-product.action.ts
@@ -7,7 +7,7 @@ export const createUpdateProductAction = async (
 ): Promise<Product> => {
   await sleep(1500);
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const { id, user, images = [], files, ...rest } = productLike; // Una destructuracion de toda la data rest es todo lo demas
+  const { id, user, images = [], files = [], ...rest } = productLike; // Una destructuracion de toda la data rest es todo lo demas
 
   console.log({ files });
   const isCreating = id === "new"; // COn ese id sabemos si el es para craear o actualizar
@@ -16,11 +16,25 @@ export const createUpdateProductAction = async (
   rest.stock = Number(rest.stock || 0);
   rest.price = Number(rest.price || 0);
 
+  // Preparar las imágenes
+  if (files.length > 0) {
+    const newImageNames = await uploadFiles(files);
+    images.push(...newImageNames);
+  }
+
+  const imagesToSave = images.map((image) => {
+    if (image.includes('http')) return image.split('/').pop() || '';
+    return image;
+  });
+
   // se pude decir url apuntar y metodo llamar porque los cuerpos son los mismos
   const { data } = await tesloApi<Product>({
     url: isCreating ? "/products" : `/products/${id}`,
     method: isCreating ? "POST" : "PATCH",
-    data: rest,
+    data: {
+      ...rest,
+      images: imagesToSave,
+    },
   });
 
   return {
@@ -31,3 +45,27 @@ export const createUpdateProductAction = async (
     }),
   };
 };
+
+export interface FileUploadResponse {
+  secureUrl: string;
+  fileName: string;
+}
+
+const uploadFiles = async (files: File[]) => {
+  const uploadPromises = files.map(async (file) => {
+    const formData = new FormData();
+    formData.append('file', file);
+
+    const { data } = await tesloApi<FileUploadResponse>({
+      url: '/files/product',
+      method: 'POST',
+      data: formData,
+    });
+
+    return data.fileName;
+  });
+
+  const uploadedFileNames = await Promise.all(uploadPromises);
+  return uploadedFileNames;
+};
+
